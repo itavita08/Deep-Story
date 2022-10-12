ef04af14a87ea42ad2c7624531143c50c8e95dd4
YeongJun <dudwns10000@gmail.com>
2022-10-12 17:59:52 +0900

    추가) 주석
    주석 추가

--- final_flask/app.py
+++ final_flask/app.py
@@ -10,9 +10,14 @@ def intro():
 
 @app.route("/click", methods=['POST'])
 def click():
-    t = request.form.get('test2')
-    #test2.saveImage(t)
-    img_src = url_for('static', filename = 'image/' + t + '.png')
+    t = request.form.get('test2')  # html에서 사용자가 입력한 text 받기
+    #test2.saveImage(t)  # 모델 실행
+    
+     # 이미지 경로
+     # <img src="url_for('static', filename = 'image/' + t + '.png')"> - js code
+     # static/image/"이미지이름".png
+    img_src = url_for('static', filename = 'image/' + t + '.png')  
+    
     return render_template('index.html', filename=img_src)
 
 
