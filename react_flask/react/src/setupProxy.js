7ac71018102e79ab8428822718a037eb0337dc90
EunHyeSeon <100753335+EunHyeSeon@users.noreply.github.com>
2022-10-12 17:59:13 +0900

    Add files via upload

---
+++ react_flask/react/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+	app.use(
+		createProxyMiddleware('/api', {
+			target: 'http://localhost:5000', 
+			changeOrigin: true,
+			pathRewrite: {
+        '^/api': '',
+      },
+		})
+	);
+};
