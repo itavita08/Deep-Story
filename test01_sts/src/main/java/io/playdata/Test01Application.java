d32840c24b1cd70459c64243bedea3926397cab0
MyungHo Lee <105653230+MyM999@users.noreply.github.com>
2022-10-12 17:52:58 +0900

    react-spring boot-test spring boot->react
    test

---
+++ test01_sts/src/main/java/io/playdata/Test01Application.java
@@ -0,0 +1,13 @@
+package io.playdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test01Application.class, args);
+	}
+
+}
