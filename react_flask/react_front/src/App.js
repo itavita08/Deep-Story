cb7da44cbde78d4bfc2c104413f04ff6a87e5312
EunHyeSeon <100753335+EunHyeSeon@users.noreply.github.com>
2022-10-13 17:07:28 +0900

    Update App.js

--- react_flask/react_front/src/App.js
+++ react_flask/react_front/src/App.js
@@ -2,47 +2,6 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 
-// class getData extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: '',
-//       playerName: ''
-//     };
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event) {
-//     this.setState({ value: event.target.value });
-//   }
-
-//   handleSubmit(event) {
-//     console.log("making request")
-//     fetch('/')
-//       .then(response => {
-//         return response.json()
-//       })
-//       .then(json => {
-//       this.setState({playerName: json[0]})
-//       })
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <form onSubmit={this.handleSubmit} action="http://127.0.0.1:5000/add" method="POST">
-//           <label>
-//             Input Text:
-//             <input type="text" name="input_text" id="input_text" />
-//             <input type="submit" onChange={this.handleChange} value={this.state.value} />
-//           </label>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
 function App() {
   // state
   const [data, setData] = useState([{}])
