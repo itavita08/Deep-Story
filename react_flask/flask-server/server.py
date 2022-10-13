0282beb16380d72817277f385a6bfbbe7b4e63e1
EunHyeSeon <100753335+EunHyeSeon@users.noreply.github.com>
2022-10-13 16:59:09 +0900

    Update server.py

--- react_flask/flask-server/server.py
+++ react_flask/flask-server/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 from flask import Flask, request
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,13 +11,22 @@ CORS(app)
 def users():
 	# users 데이터를 Json 형식으로 반환한다
     return {"users": [{ "id" : 1, "name" : "hyeseon" },
-    					{ "id" : 2, "name" : "dalkong" }]}
-         
+    					{ "id" : 2, "name" : "from flask" }]}         
 
 @app.route("/add", methods=["POST"], strict_slashes=False)
 def add_text():
-    data = request.form.get('input_text')
-    return data
+    text = request.form.get('input_text')
+    #test2.saveImage(text)
+
+    # 이미지 경로
+     # <img src="url_for('static', filename = 'image/' + t + '.png')"> - js code
+     # static/image/"이미지이름".png
+      
+    return f'''
+    <div>
+        <img src="./static/image/{text}.png">
+    </div>
+    '''
 
 if __name__ == "__main__":
     app.run(debug = True)
