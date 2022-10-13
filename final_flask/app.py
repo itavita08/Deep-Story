27ac720851324e20203ac13f53b8eb2bbce8aedf
SOYOUNG CHOI <cso6005@naver.com>
2022-10-13 17:08:17 +0900

    Update app.py
    수정 app.py (flask 연동 추가)

--- final_flask/app.py
+++ final_flask/app.py
@@ -10,15 +10,21 @@ def intro():
 
 @app.route("/click", methods=['POST'])
 def click():
-    t = request.form.get('test2')  # html에서 사용자가 입력한 text 받기
-    #test2.saveImage(t)  # 모델 실행
+
+    data = request.form.get('input_text')
+    # print(data)
+    #test2.saveImage(data)  # 모델 실행
     
-     # 이미지 경로
-     # <img src="url_for('static', filename = 'image/' + t + '.png')"> - js code
-     # static/image/"이미지이름".png
-    img_src = url_for('static', filename = 'image/' + t + '.png')  
+    # 이미지 경로
+    # <img src="url_for('static', filename = 'image/' + t + '.png')"> - js code
+    # static/image/"이미지이름".png
+    #img_src = url_for('static', filename = 'image/' + data + '.png')  
+    img_src = url_for('static', filename = 'image/' + data + '.png')  
     
-    return render_template('index.html', filename=img_src)
+    
+    # return render_template('../src/App.js', filename=img_src)
+    # return render('/src/App.js', filename=img_src)
+    return img_src
 
 
 if __name__ == "__main__":
