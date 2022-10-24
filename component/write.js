44da8f64b099329637fadcba3aae645d0dea3ab7
SilverSunLine <100753335+EunHyeSeon@users.noreply.github.com>
2022-10-24 19:05:46 +0900

    Update write.js

--- component/write.js
+++ component/write.js
@@ -1,14 +1,12 @@
 import React, { useState, handleSubmit } from 'react';
-import LoadingSpinner from "../Components/Loader";
 import { Input } from './index.js';
 import axios from 'axios';
 
 
-
 function write() {
   const [value, setValue] = useState(null);
-
-  const _submitBoard = async ()=> {
+ 
+  const _submitBoard = async () => {
     const title = document.getElementsByName('title')[0].value.trim();
     const contents = document.getElementsByName('contents')[0].value.trim();
 
@@ -19,46 +17,28 @@ function write() {
       return alert('내용을 입력해주세요.');
     }
 
-    const data = { title : title, contents : contents };
-    const res = await axios('/add/test', {
-      method : 'POST',
-      data : data,
-      headers: new Headers()
+    const data = { title : title, contents : contents, image : value};
+    const res = await axios.post('/api/test', data, {  
+      headers: {
+        'content-type': 'multipart/form-data'
+      }, 
     })
   }
-
-
-  // const handleSubmit =(event) => {
-  //   event.preventDefault();
-  //    fetch("/api/test", {
-  //      method: 'POST',
-  //      mode:"cors",
-  //      headers:{
-  //       'content-type':'multipart/form-data'
-  //      },
-  //     body: JSON.stringify(event.target.board.value)
-  //    })
-  //    .then(response => response.json())
-  //      .then(value => {
-  //        setValue(value)
-  //      })
-  //    .catch(e => alert(1+ ' ' + e))
-  //   };
-    
     
     return (
       <div className='Write'>   
         <div>
-          <Input/>
+          <Input onSave = {(data) => setValue(data)}/>
         </div>    
-        <form name='board' id='board_form' onSubmit={handleSubmit}>
+        <form id='board_form'>
         <input type='text' autoComplete='off' id='title_txt' name='title' placeholder='제목'/>
         <textarea id='content_txt' name='contents' placeholder='내용을 입력하세요.'></textarea>
         <button onClick={() => _submitBoard()}> 포스트 등록 </button>
-        </form>
+        </form>  
       </div>
     );
   
 }
 
 export default write;
+
