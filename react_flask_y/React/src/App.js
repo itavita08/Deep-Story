172864cbbd8636e392a97253211a33bf10b80802
YeongJun <dudwns10000@gmail.com>
2022-10-24 14:50:04 +0900

    [게시물생성] 이미지 슬라이드
    이미지 1장일때는 슬라이드 x
    여러장일때 슬라이드

--- react_flask_y/React/src/App.js
+++ react_flask_y/React/src/App.js
@@ -1,76 +1,37 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import { useState, useEffect } from 'react';
-// import { useState } from 'react';
-
-
+import { useState } from 'react';
+import Test from './component/imageLoad';
+import Carousel from 'react-material-ui-carousel'
 
 
 function App() {
-  // state
-  const [data, setData] = useState([{}])
-  const [value, setValue] = useState(null);
   const [content, setContent] = useState(false);
+  const [newList, setNewList] = useState([])
 
   
   let content2 = null;
 
- const handleSubmit = (event) => {
-  event.preventDefault();
-   fetch("http://127.0.0.1:5000/add", {
-     method: 'POST',
-     mode:"cors",
-     headers:{
-      'content-type':'application/json'
-     },
-    body: JSON.stringify(event.target.inputText.value)
-   })
-   .then(response => response.json())
-     .then(value => {
-       setValue(value)
-        setContent(true) 
-     })
-   .catch(e => alert(1+ ' ' + e))
-  }
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/users").then(
-      response => response.json()
-    ).then(
-      data => {
-        setData(data);
-      }
-    ).catch(
-      (err) => console.log(err)
-    )
-  }, []);
-
   if(content === true){
-    content2 = <img src={"/static/image/"+value+".png"}/>
-  }
+    if(newList.length <= 1){
+      content2 = newList.map(v => <img key={v.name} src={"/static/image/"+v.name+".png"}/>)
+    } else{
+      content2 = <Carousel>{newList.map( v => <img key={v.name} src={"/static/image/"+v.name+".png"}/>)}</Carousel>
+    }
+  } 
   
   return (
     <div className='App'>
-      <h1>test 하는 중...</h1>
-      <div>
-        {(typeof data.users === 'undefined') ? (
-          <p>loding...</p>
-        ) : (
-          data.users.map((u) => <p>{u.name}</p>)
-        )}
-      </div >
-          <form onSubmit={handleSubmit}>
-            <label>
-              Input Text:
-              <input type="text" name="inputText" id="inputText" />
-              <input type="submit" value="버튼" />
-            </label>
-          </form>
-      
-          <h1>{value}</h1>
-            {content2}  
+          <Test onCreate={(v)=>{
+            const testList = {name:v}
+              const testList2 = [...newList]
+              testList2.push(testList);
+              setNewList(testList2);
+              setContent(true);
+          }}></Test>
+          {content2}
     </div >
   )
 };
 
-export default App;
+export default React.memo(App);
