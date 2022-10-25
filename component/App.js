6f51c0551e2398d2e8398228073275f92015034e
YEONG JUN KIM <105635205+itavita08@users.noreply.github.com>
2022-10-25 11:55:17 +0900

    Update App.js

--- component/App.js
+++ component/App.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import { useState} from 'react';
 import Test from './component/imageLoad';
+import Write from './component/write'
+import ImageList from './component/printImage'
+import './App.css'
 
 
 function App() {
   const [content, setContent] = useState(false);
   const [newList, setNewList] = useState([])
-
   
-  let content2 = null;
-
-  if(content === true){
-    // eslint-disable-next-line jsx-a11y/alt-text
-    content2 = newList.map(v => <img key={v.name} src={"/static/image/"+v.name+".png"}/>)
-    console.log(newList);
-  } 
+  const onDelete = (targetId) => {
+  const newReportList = newList.filter((it) => it.name !== targetId);	//filter 메소드 사용!!
+  setNewList(newReportList);
+  };
   
   return (
     <div className='App'>
+          <div className='image'>
           <Test onCreate={(v)=>{
             const testList = {name:v}
               const testList2 = [...newList]
               testList2.push(testList);
               setNewList(testList2);
-              setContent(true);
+              setContent1(true);
           }}></Test>
-          {content2}
+          <ImageList data={newList} onDelete={onDelete}/>
+      </div>
+          <Write onAccess={(title, contents)=>{
+            return {'title':title, 'contents':contents, 'image':newList}
+          }}></Write>
     </div >
   )
 };
