90f21db1887d8d0c26c98858b4bfb4d2389c3e29
YeongJun <dudwns10000@gmail.com>
2022-10-26 14:11:49 +0900

    Flask Update

--- react_flask_y/Flask/app.py
+++ react_flask_y/Flask/app.py
@@ -6,13 +6,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-CORS(app)
-
-@app.route('/users')
-def users():
-	# users 데이터를 Json 형식으로 반환한다
-    return {"users": [{ "id" : 1, "name" : "hyeseon" },
-    					{ "id" : 2, "name" : "from flask" }]}         
+CORS(app)       
 
 # @app.route("/add", methods=["POST"], strict_slashes=False)
 @app.route("/add", methods=["POST"])
